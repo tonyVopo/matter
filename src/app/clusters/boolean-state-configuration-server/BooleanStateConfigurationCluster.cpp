bc1bdf5f7ec9966413c27f77ed9c72f7b8cabcd7
Restyled.io <commits@restyled.io>
2026-02-09 04:40:05 +0000

    Restyled by clang-format

--- src/app/clusters/boolean-state-configuration-server/BooleanStateConfigurationCluster.cpp
+++ src/app/clusters/boolean-state-configuration-server/BooleanStateConfigurationCluster.cpp
@@ -195,8 +195,8 @@ BooleanStateConfigurationCluster::InvokeCommand(const DataModel::InvokeRequest &
         if (mAlarmsSuppressed.HasAny(alarmsToDisable))
         {
             mAlarmsSuppressed.Clear(alarmsToDisable);
-            NotifyAttributeChangedAndCallDelegate(AlarmsSuppressed::Id,
-                                                  [this](auto * delegate) { delegate->OnAlarmsSuppressedChanged(mAlarmsSuppressed); });
+            NotifyAttributeChangedAndCallDelegate(
+                AlarmsSuppressed::Id, [this](auto * delegate) { delegate->OnAlarmsSuppressedChanged(mAlarmsSuppressed); });
             generateEvent = true;
         }
 
@@ -311,8 +311,9 @@ CHIP_ERROR BooleanStateConfigurationCluster::SetCurrentSensitivityLevel(uint8_t 
     VerifyOrReturnError(mCurrentSensitivityLevel != level, CHIP_NO_ERROR);
 
     mCurrentSensitivityLevel = level;
-    NotifyAttributeChangedAndCallDelegate(CurrentSensitivityLevel::Id,
-                                          [this](auto * delegate) { delegate->OnCurrentSensitivityLevelChanged(mCurrentSensitivityLevel); });
+    NotifyAttributeChangedAndCallDelegate(CurrentSensitivityLevel::Id, [this](auto * delegate) {
+        delegate->OnCurrentSensitivityLevelChanged(mCurrentSensitivityLevel);
+    });
 
     // TODO: we should migrate this to not use `Safe` attribute persistence and use
     //       a common persistence layer.
--- src/app/clusters/boolean-state-configuration-server/tests/TestBooleanStateConfigurationCluster.cpp
+++ src/app/clusters/boolean-state-configuration-server/tests/TestBooleanStateConfigurationCluster.cpp
@@ -539,7 +539,7 @@ private:
     bool mSensorFaultCalled = false;
     chip::BitMask<BooleanStateConfiguration::SensorFaultBitmap> mSensorFaultValue{};
 
-    bool mSuppressAlarmCalled = false;
+    bool mSuppressAlarmCalled                                      = false;
     BooleanStateConfiguration::AlarmModeBitmap mSuppressAlarmValue = BooleanStateConfiguration::AlarmModeBitmap::kVisual;
 
     bool mEnableDisableAlarmsCalled = false;
